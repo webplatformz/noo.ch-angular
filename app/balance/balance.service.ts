293d72799f473453ea84cbfe5651f4962be14e1a
Ben Millo <bmi@zuehlke.com>
2016-06-08 11:17:57 +0200

    hardcoded prod url. Changed path of user import

--- app/balance/balance.service.ts
+++ app/balance/balance.service.ts
@@ -10,7 +10,7 @@ export class BalanceService {
 
     constructor(private http: Http) { }
 
-    private balanceUrl = 'http://localhost:3042/api/plans/mockPlanId/balance';
+    private balanceUrl = 'http://noo.ch:8080/api/plans/mockPlanId/balance';
 
     getBalance(): Observable<Balance> {
         return this.http.get(this.balanceUrl)
